99530cbd769559caf2f986d8400f5fc5519c8308
agostinopique <agostino.pique@gmail.com>
2022-09-19 16:55:24 +0200

    Corretti nomi delle variabili;

--- Program.cs
+++ Program.cs
@@ -2,27 +2,32 @@
 Console.WriteLine("Hello, Lo-Fi!");
 
 
-
 Console.WriteLine("Inserisci il nome di un prodotto: ");
 string productName = Console.ReadLine();
+
 
 Console.WriteLine("Inserisci il prezzo del prodotto: ");
 double productPrice = Convert.ToDouble(Console.ReadLine());
 
+
 Console.WriteLine("Inserisci l'IVA del prodotto: ");
 int iva = Convert.ToInt32(Console.ReadLine());
 
+
 Product product = new Product(productName, productPrice,iva);
+
 
 Console.WriteLine("Prodotto inserito correttamente!");
 
-//Console.WriteLine(product.GetCorrectCode());
 
-Console.WriteLine(product.GetProductName() + "  " + product.GetCorrectCode());
+Console.WriteLine(product.GetName() + "  " + product.GetCorrectCode());
+
 
 Console.WriteLine("Vuoi avere il prezzo del prodotto con l'IVA? (si/no)");
 
+
 string answer = Console.ReadLine();
+
 
 switch (answer)
 {
@@ -36,4 +41,3 @@ switch (answer)
         Console.WriteLine("Il prezzo senza IVA é quello inserito da te in precedenza!");
         break;
 }
-
