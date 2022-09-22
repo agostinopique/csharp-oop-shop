f859969484622abf46b8d5ebc71dd97508f662f2
agostinopique <agostino.pique@gmail.com>
2022-09-22 15:57:59 +0200

    First commit; Aggiunte sottoclassi per prodotti in base al prodotto inserito;

--- Acqua.cs
+++ Acqua.cs
@@ -117,39 +117,59 @@
 
 #endregion old excercise
 
-public abstract class Product
+using System.Diagnostics.Contracts;
+
+public class Acqua : Product
 {
-    public int Code { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public double Price { get; set; }
-    public int IVA { get; set; }
+    public string Origin { get; set; }
+    public double Ph { get; set; }
+    public double Liters { get; set; }
+    public bool Sparkling { get; set; }
 
-
-    // DEFAULT CONSTRUCTOR
-    public Product()
+    public Acqua(string name, double price, int iva, string origin, double ph, double liters, bool sparkling) : base(name, price, iva)
     {
-        this.Code = new Random().Next(99999999);
-        this.Name = null;
-        this.Description = null;
-        this.Price = 0;
-        this.IVA = 0;
+        this.Origin = origin;
+        this.Ph = ph;
+        this.Liters = liters;
+        this.Sparkling = sparkling;
     }
 
-    // COMPLEX CONSTRUCTOR
-    public Product(string name, double price, int iva)
+    //  - metodo bevi (double litriDaBere) che toglie dall’attributo litri una quantità pari a litriDaBere di acqua se presenti.
+    //  - metodo riempi(double litri) che riempie la bottiglia di acqua con tot litri di acqua, stando attenti a non riempirla troppo.
+    //  - metodo svuota() che rimuove tutta l’acqua dalla bottiglia.
+    //  - metodo stampaProdotto() che fa l’override del metodo di base StampaProdotto già dichiarato nella superclasse
+
+    public void empty()
     {
-        this.Code = new Random().Next(999999);
-        this.Name = name;
-        this.Description = null;
-        this.Price = price;
-        this.IVA = iva;
+
+    }
+    public double drink(double poured)
+    {
+        double waterLeft = this.Liters - poured;
+
+        if(waterLeft < 0)
+        {
+            Console.WriteLine("You cant drink this much water.");
+            Console.WriteLine("You can only drink " + Liters + " amount of water");
+            return Liters;
+        }
+        return this.Liters - poured;
+    }
+
+    public double fill(double liters)
+    {
+        double filled = this.Liters + liters;
+
+        if (filled > 1.5)
+        {
+            Console.WriteLine("You can fill the bottle more than its maximum capacity!");
+            return filled - Liters;
+        }
+        else
+        {
+            Console.WriteLine("You filled your bottle!");
+            this.Liters = filled;
+            return filled;
+        }
     }
 }
-
-
-//public class Frutta : Product
-//{
-//    public string Variety { get; set; }
-//    public 
-//}
--- Program.cs
+++ Program.cs
@@ -1,43 +1,87 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+//A partire da quanto già fatto con csharp-oop-shop dove vi era stato chiesto di creare una classe Prodotto generica per gestire un generico prodotto dello Shop, vi chiedo di essere più specifici e di creare le sottoclassi opportune di alcuni prodotti che potremmo identificare nel nostro shop.
+
+//In particolare immaginiamo che nel nostro shop possiamo commerciare le seguenti tipologie di prodotto:
+//  - Acqua(massimo 1.5L)
+//  - Sacchetto di Frutta a pezzi (al massimo puo contenere 5 pezzi di frutta)
+//  - Elettrodomestico
+//  - Cibo in Scatola (al massimo può contenere 300g di peso su una scatola)
+
+//Per ognuno dei seguenti prodotti più specifici vi si chiede di identificare quali potrebbero essere i loro attributi e metodi che eventualmente gli riguardano, ereditando i metodi e gli attributi generici dalla classe Prodotto creata nell’esercizio precedente.
+
+//Un esempio per la classe Acqua che eredità Prodotto è aggiungere i seguenti attributi e metodi:
+//  - attributo litri: per indicare quanti litri di acqua ci sono nella bottiglia di acqua
+//  - attributo ph: che indica il livello di acidità dell’acqua (sola lettura)
+//  - attributo sorgente: che indica la sorgente di provenienza dell’acqua (sola lettura)... ecc ecc
+//  - metodo bevi (double litriDaBere) che toglie dall’attributo litri una quantità pari a litriDaBere di acqua se presenti.
+//  - metodo riempi(double litri) che riempie la bottiglia di acqua con tot litri di acqua, stando attenti a non riempirla troppo.
+//  - metodo svuota() che rimuove tutta l’acqua dalla bottiglia.
+//  - metodo stampaProdotto() che fa l’override del metodo di base StampaProdotto già dichiarato nella superclasse
+//  Prodotto (se non lo avete già fatto dichiaratelo anche nella classe prodotto è si occupa di stampare il “codice - nome”, l lo abbiamo chiamato NomeEsteso), per stampare oltre al codice e al nome dell’acqua, anche la sua sorgente, il ph e i litri contenuti.
+//  - un metodo statico convertiInGalloni(double litri) che presa una quantità di litri restituisca la conversione dei litri in galloni, sapendo che 1 litro è equivalente a 3,785 galloni (ricordatevi di codificare le costanti come 3.785 nel modo corretto come visto in classe).
+
+//Ricordatevi di creare questi metodi in maniera responsabile, in modo che non rendono “incoerente” lo stato dei vostri oggetti (ad esempio non posso bere più dei litri contenuti nella bottiglia, o non posso riempire più di tot una bottiglia!).
+
+//E così via per tutti gli altri elementi qui sopra riportati, provate a pensare attributi e metodi che potrebbero servire ai vostri prodotti specializzati Acqua, sacchetto di frutta a pezzi, elettrodomestici e cibo in scatola.
+
+//Al solito usate opportunamente i livelli di accesso (public, private, protected o altro se necessario), i costruttori, i metodi getter e setter (o le properties) ed eventuali altri metodi di “utilità“.
+
+//Pensate anche se avete bisogno di eventuali metodi statici o attributi statici (ad esempio potrebbero essere i limiti dei vostri oggetti ), e ad eventuali override e overload di metodi come visto per Acqua.
+
+//Una volta finite più o meno le vostre classi, nel Program.cs, provate a testare un pò i vostri prodotti specializzati, osservando come i metodi della classe generica prodotto e quelli più specifici vengono richiamati e siano disponibili.
+
+
+
+using System.Diagnostics;
+using System.Xml.Linq;
+
 Console.WriteLine("Hello, Lo-Fi!");
 
-
-Console.WriteLine("Inserisci il nome di un prodotto: ");
-string productName = Console.ReadLine();
-
-
-Console.WriteLine("Inserisci il prezzo del prodotto: ");
-double productPrice = Convert.ToDouble(Console.ReadLine());
+#region Old excercise
+//Console.WriteLine("Inserisci il nome di un prodotto: ");
+//string productName = Console.ReadLine();
 
 
-Console.WriteLine("Inserisci l'IVA del prodotto: ");
-int iva = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Inserisci il prezzo del prodotto: ");
+//double productPrice = Convert.ToDouble(Console.ReadLine());
 
 
-Product product = new Product(productName, productPrice, iva);
+//Console.WriteLine("Inserisci l'IVA del prodotto: ");
+//int iva = Convert.ToInt32(Console.ReadLine());
 
 
-Console.WriteLine("Prodotto inserito correttamente!");
+//Product product = new Product(productName, productPrice, iva);
 
 
-Console.WriteLine(product.GetName() + "  " + product.GetCorrectCode());
+//Console.WriteLine("Prodotto inserito correttamente!");
 
 
-Console.WriteLine("Vuoi avere il prezzo del prodotto con l'IVA? (si/no)");
+//Console.WriteLine(product.GetName() + "  " + product.GetCorrectCode());
 
 
-string answer = Console.ReadLine();
+//Console.WriteLine("Vuoi avere il prezzo del prodotto con l'IVA? (si/no)");
 
 
-switch (answer)
-{
-    case "si":
-        Console.WriteLine(product.GetFullPrice());
-        break;
-    case "no":
-        Console.WriteLine("OK");
-        break;
-    default:
-        Console.WriteLine("Il prezzo senza IVA é quello inserito da te in precedenza!");
-        break;
-}
+//string answer = Console.ReadLine();
+
+
+//switch (answer)
+//{
+//    case "si":
+//        Console.WriteLine(product.GetFullPrice());
+//        break;
+//    case "no":
+//        Console.WriteLine("OK");
+//        break;
+//    default:
+//        Console.WriteLine("Il prezzo senza IVA é quello inserito da te in precedenza!");
+//        break;
+//}
+
+#endregion Old excercise
+
+Acqua levissima = new Acqua("Levissima", 1.75, 4, "Sorgenti del trentino", 0.4, 1.5, false);
+
+Console.WriteLine(levissima.Name + " " + levissima.Origin);
